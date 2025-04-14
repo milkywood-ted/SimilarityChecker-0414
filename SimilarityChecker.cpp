4ad64079d05217072c4423a2e4171767e3b91fa5
Ted KIm <milkywood@gmail.com>
2025-04-14 10:30:47 +0900

    feature/existence : refactoring

--- SimilarityChecker.cpp
+++ SimilarityChecker.cpp
@@ -4,50 +4,45 @@ using std::string;
 
 class SimilarityChecker {
 public:
-	int lengthSimilarity(string& first, string& second) {
+	int lengthSimilarity(const string& first, const string& second) {
 		if (first.length() >= second.length() * 2 || second.length() >= first.length() * 2)
 			return 0;
 		if (first.length() == second.length())
 			return 60;
 
-		return getPartialScore(first, second);
+		return getPartialScoreLength(first, second);
 	}
 
-
-	int existenceSimilarity(string& first, string& second) {
+	int existenceSimilarity(const string& first, const string& second) {
 		if (false == isUpper(first) || false == isUpper(second))
 			return 0;
-		if (existSameLetters(first, second))
+
+		updateAlphabets(first, second);
+		if (existSameLetters())
 			return 40;
 
-		//partialScore = samecnt * 40 / totalcnt
-		int sameCnt = 0, totalCnt = 0;
-		bool alphabetsFirst[26] = { 0 }, alphabetsSecond[26] = { 0 };
+		return getPartialScoreExistence();
+	}
+
+private:
+	bool alphabetsFirst[26] = { 0 }, alphabetsSecond[26] = { 0 };
+
+	void updateAlphabets(const string& first, const string &second) {
 		for (auto letter : first) {
 			alphabetsFirst[letter - 'A'] = true;
 		}
 		for (auto letter : second) {
 			alphabetsSecond[letter - 'A'] = true;
 		}
-
-		for (int i = 0; i < 26; ++i) {
-			if (alphabetsFirst[i] && alphabetsSecond[i]) sameCnt++;
-			if (alphabetsFirst[i] || alphabetsSecond[i]) totalCnt++;
-		}
-
-		return sameCnt * 40 / totalCnt;
 	}
 
-private:
-
-	bool isUpper(string& str) {
+	bool isUpper(const string& str) const {
 		for (auto letter : str)
 			if (letter < 'A' || letter > 'Z') return false;
 		return true;
 	}
 
-	int getPartialScore(std::string& first, std::string& second)
-	{
+	int getPartialScoreLength(const std::string& first, const std::string& second) const {
 		int firstLength = first.length(), secondLength = second.length();
 		int lengthDiff = firstLength - secondLength;
 		int shorterLength = secondLength;
@@ -57,22 +52,26 @@ private:
 			shorterLength = firstLength;
 		}
 
-		int partialScore = 60 - (60 * lengthDiff) / shorterLength;
-
-		return partialScore;
+		return 60 - (60 * lengthDiff) / shorterLength;
 	}
 
-	bool existSameLetters(string& first, string& second) {
-		bool alphabets[26] = { false };
-
-		for (auto letter : first) {
-			alphabets[letter - 'A'] = true;
-		}
-		for (auto letter : second) {
-			if (alphabets[letter - 'A'] == false)
+	bool existSameLetters() const {
+		for (int i = 0; i < 26; ++i) {
+			if (alphabetsFirst[i] ^ alphabetsSecond[i])
 				return false;
 		}
 
 		return true;
+	}
+
+	int getPartialScoreExistence() const {
+		int sameCnt = 0, totalCnt = 0;
+
+		for (int i = 0; i < 26; ++i) {
+			if (alphabetsFirst[i] && alphabetsSecond[i]) sameCnt++;
+			if (alphabetsFirst[i] || alphabetsSecond[i]) totalCnt++;
+		}
+
+		return sameCnt * 40 / totalCnt;
 	}
 };
